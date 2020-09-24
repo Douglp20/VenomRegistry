98d94d6674f448dde4494f584e6e2e452615202d
Douglp20 <philmore.douglas@virginmedia.com>
2020-09-24 20:44:38 +0100

    Add project files.

---
+++ VenomRegistry/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("VenomRegistry")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("VenomRegistry")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("8987f4b9-243f-48b9-9c93-d42a0c237a16")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.1")>
+<Assembly: AssemblyFileVersion("1.0.0.1")>
---
+++ VenomRegistry/VenomRegistry.vb
@@ -0,0 +1,86 @@
+﻿Public Class VenomRegistry
+    Public Event ErrorMessage(ByVal errDesc As String, ByVal errNo As Integer, ByVal errTrace As String)
+    Public Sub New()
+    End Sub
+    Private Function getFullPath(KeySection As String) As String
+        Dim path As String = "Software\\VB and VBA Program Settings\\circuit\" + KeySection
+        Return path
+
+    End Function
+    Public Sub SaveSetting(keySection As String, keyString As String, keySetting As String)
+        On Error GoTo Err
+
+        Dim key As Microsoft.Win32.RegistryKey
+        key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(getFullPath(keySection), True)
+
+        If key Is Nothing Then
+            key = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(getFullPath(keySection))
+        End If
+        key.SetValue(keyString, keySetting)
+
+        Exit Sub
+Err:
+
+        Dim rtn As String = "The error occur within the module " + System.Reflection.MethodBase.GetCurrentMethod().Name + " : " + Me.ToString() + "."
+        RaiseEvent ErrorMessage(Err.Description, Err.Number, rtn)
+
+    End Sub
+    Public Function GetSetting(keySection As String, keyString As String) As String
+
+        On Error GoTo Err
+
+        Dim strName As String
+        Dim key As Microsoft.Win32.RegistryKey
+
+        key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(getFullPath(keySection), True)
+
+        If key Is Nothing Then
+            strName = ""
+        Else
+            If key.GetValue(keyString, "").ToString = "" Then
+                strName = ""
+            Else
+                strName = key.GetValue(keyString).ToString
+            End If
+        End If
+        Return strName.ToString
+
+        Exit Function
+Err:
+
+        Dim rtn As String = "The error occur within the module " + System.Reflection.MethodBase.GetCurrentMethod().Name + " : " + Me.ToString() + "."
+        RaiseEvent ErrorMessage(Err.Description, Err.Number, rtn)
+
+    End Function
+    Public Function GetSetting(keySection As String, keyString As String, keyDefault As String) As String
+
+        On Error GoTo Err
+        Dim strName As String
+        Dim key As Microsoft.Win32.RegistryKey
+
+
+        key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(getFullPath(keySection), True)
+
+        If key Is Nothing Then
+            strName = keyDefault
+        Else
+            If key.GetValue(keyString, "").ToString = "" Then
+                strName = keyDefault
+            Else
+                Select Case key.GetValue(keyString).GetType.ToString
+                    Case "System.String"
+                        strName = key.GetValue(keyString).ToString
+                    Case Else
+                        strName = keyDefault
+                End Select
+
+            End If
+        End If
+        Return strName.ToString
+        Exit Function
+Err:
+
+        Dim rtn As String = "The error occur within the module " + System.Reflection.MethodBase.GetCurrentMethod().Name + " : " + Me.ToString() + "."
+        RaiseEvent ErrorMessage(Err.Description, Err.Number, rtn)
+    End Function
+End Class
